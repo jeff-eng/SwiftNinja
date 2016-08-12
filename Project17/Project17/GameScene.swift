30920fbcfa1a7f87fbfecdb281aff82ce9b2421f
Jeff Eng <jeff@jeffeng.me>
2016-08-11 17:51:15 -0700

    added in project files, helper file, deleted template code, and set project to landscape only

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Jeffrey Eng on 8/11/16.
+//  Copyright (c) 2016 Jeffrey Eng. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
