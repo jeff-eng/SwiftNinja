6aa83420dce097faa0fcbe11e4d580ec44bbd3c1
Jeff Eng <jeff@jeffeng.me>
2016-08-12 14:33:11 -0700

    added background and defined the gravity property physics

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -10,7 +10,25 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-
+        // Create instance of sprite node
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        // Position sprite
+        background.position = CGPoint(x: 512, y: 384)
+        // Replace over scene
+        background.blendMode = .Replace
+        // Specify the z-position
+        background.zPosition = -1
+        // Add sprite node as child of the scene
+        addChild(background)
+        
+        // Define gravity
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
