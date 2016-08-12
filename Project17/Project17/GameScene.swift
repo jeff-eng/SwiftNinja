1619cd057167ba23f9600bd0c8607777c0cc0996
Jeff Eng <jeff@jeffeng.me>
2016-08-12 16:35:52 -0700

    added the score label node and the lives nodes to the scene

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -9,6 +9,17 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var gameScore: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
     override func didMoveToView(view: SKView) {
         // Create instance of sprite node
         let background = SKSpriteNode(imageNamed: "sliceBackground")
@@ -23,6 +34,7 @@ class GameScene: SKScene {
         
         // Define gravity
         physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        // Set speed of gravity slightly slower than default
         physicsWorld.speed = 0.85
         
         createScore()
@@ -38,5 +50,37 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func createScore() {
+        // Create instance of label node and specify the text, alignment and font size
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .Left
+        gameScore.fontSize = 48
+        
+        // Add label node to the scene
+        addChild(gameScore)
+        
+        // Specify the position of the game score label
+        gameScore.position = CGPoint(x: 8, y: 8)
+    }
+    
+    func createLives() {
+        for i in 0..<3 {
+            // create instance of the sprite node
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            // position the sprite node
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            // add the sprite node as a child node to the scene
+            addChild(spriteNode)
+            
+            // add the sprite node object to the array declared at the top of the class
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices() {
+        
     }
 }
